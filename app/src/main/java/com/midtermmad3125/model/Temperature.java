6367b248e6706f2f462b3813b4d4cd621ca8ab1b
harsh1395 <patelharsh1395@gmail.com>
2019-04-09 01:42:10 -0400

    ..

--- app/src/main/java/com/midtermmad3125/model/Temperature.java
+++ app/src/main/java/com/midtermmad3125/model/Temperature.java
@@ -1,11 +1,7 @@
 package com.midtermmad3125.model;
 
 
-//        "min": 10.35,
-//        "max": 10.35,
-//        "night": 10.35,
-//        "eve": 10.35,
-//        "morn": 10.35
+     "morn": 10.35
 
 
 import java.io.Serializable;
